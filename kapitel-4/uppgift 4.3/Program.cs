9b347406992eb09ebeef70b025910dbbc6d2db1b
Leo Tuulse <Leo.tuulse@elev.ga.ntig.se>
2021-10-12 12:15:40 +0200

    uppgift4.11

---
+++ kapitel-4/uppgift 4.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace uppgift_4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Vem är rikast i världen");
+            string svar = Console.ReadLine();
+            int antalGissningar = 1;
+
+            while (svar != "elon musk")
+            {
+                if (antalGissningar >= 5)
+                {
+                    Console.WriteLine("Dina gissningar är slut");
+                    break;
+                }
+                Console.WriteLine("Fel svar, försök igen");
+                svar = Console.ReadLine();
+                antalGissningar++;
+            }
+
+            if (svar == "elon musk")
+            {
+                Console.WriteLine("Ditt svar är rätt!");
+            }
+        }
+    }
+}
