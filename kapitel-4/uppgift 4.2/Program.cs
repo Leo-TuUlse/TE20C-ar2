5ac7dec0f4a1432ccec0526615d514255286c8b0
Leo Tuulse <Leo.tuulse@elev.ga.ntig.se>
2021-10-12 12:11:02 +0200

    Uppgift4.2

---
+++ kapitel-4/uppgift 4.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uppgift_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 50;
+            while (i > 0)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+
+        }
+    }
+}
